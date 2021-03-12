3850db8e7e1e3069b32f3ea2611cc26caa23eced
Tu Nombre <Berenice>
2021-03-11 22:48:18 -0600

    Nodo posición + nodos: pid1 y pid2

--- lidar_plugin.cc
+++ lidar_plugin.cc
@@ -29,23 +29,20 @@ namespace gazebo
 
 	  // Store the model pointer for convenience.
 	  this->model = _model;
+	  //Set the initial position
+	  this->Message.Set(0.0,0.0);
 
-	  // Get the first joint. We are making an assumption about the model
-	  // having one joint that is the rotational joint.
+	  // Get the first joint.
 	  this->joint1 = _model->GetJoints()[0];
 	  this->pid1 = common::PID(3, 5, 0);
-	  //this->GetErrors = common::GetErrors (20,20,10);
-	  
-	  //std::cerr << this->pid1::GetErrors (20,20,10);
 	  
 	  this->model->GetJointController()->SetPositionPID(
 	      this->joint1->GetScopedName(), this->pid1);
 
 	  this->model->GetJointController()->SetPositionTarget(
-	      this->joint1->GetScopedName(), 0.0);
-	  /*    std::cerr << "Joint1 ";
-	      std::cerr << joint1->Position() << "\r\n";*/
-	      
+	      this->joint1->GetScopedName(), this->Message[0]);
+
+	  // Get the second joint.    
 	  this->joint2 = _model->GetJoints()[1];
 	  this->pid2 = common::PID(60, 60, 0);
 	  
@@ -53,26 +50,7 @@ namespace gazebo
 	      this->joint2->GetScopedName(), this->pid2);
 
 	  this->model->GetJointController()->SetPositionTarget(
-	      this->joint2->GetScopedName(), 0.0);
-	  /*    std::cerr << "Joint2: ";
-	      std::cerr << joint2->Position() << "\r\n";*/
-	  
-	  /*double position1 = 0;
-	  double position2 = 0;
-	  
-	  if (_sdf->HasElement("position1"))
-	  position1 = _sdf->Get<double>("position1");
-
-	//this->model->GetJointController()->SetPositionTarget(
-	    //this->joint->GetScopedName(), position1);   
-	    
-	  if (_sdf->HasElement("position2"))
-	  position2 = _sdf->Get<double>("position2");*/
-
-	//this->model->GetJointController()->SetPositionTarget(
-	    //this->joint->GetScopedName(), position2);  
-	  //this ->SetPosition(position1); 
-	  //this ->SetPosition(position2); 
+	      this->joint2->GetScopedName(), this->Message[1]);
 	
 	// Create the position node
 	this->nodePos = transport::NodePtr(new transport::Node());
@@ -109,7 +87,12 @@ namespace gazebo
 	this->subPID2 = this->nodePID->Subscribe(topicNamePID2,
 	   &LidarPlugin::OnMsgPID2, this); 
 
-	// Initialize ros, if it has not already bee initialized.
+	// Listen to the update event. This event is broadcast every
+    // simulation iteration.
+	this->updateConnection = event::Events::ConnectWorldUpdateBegin(
+          std::bind(&LidarPlugin::OnUpdate, this));   
+
+	// Initialize ros, if it has not already been initialized.
 	if (!ros::isInitialized())
 	{
 	  int argc = 0;
@@ -118,18 +101,8 @@ namespace gazebo
 	      ros::init_options::NoSigintHandler);
 	}
 
-	// Create our ROS node. This acts in a similar manner to
-	// the Gazebo node
+	// Create our ROS node.
 	this->rosNode.reset(new ros::NodeHandle("gazebo_client"));
-
-	// Create a named topic, and subscribe to it.
-	/*ros::SubscribeOptions so =
-	  ros::SubscribeOptions::create<std_msgs::Float32>(
-	      "/" + this->model->GetName() + "/pos_cmd",
-	      1,
-	      boost::bind(&LidarPlugin::OnRosMsg, this, _1),
-	      ros::VoidPtr(), &this->rosQueue);
-	this->rosSub = this->rosNode->subscribe(so);*/
 	
 	ros::SubscribeOptions so =
 	  ros::SubscribeOptions::create<std_msgs::Float32MultiArray>(
@@ -142,32 +115,14 @@ namespace gazebo
 	// Spin up the queue helper thread.
 	this->rosQueueThread =
 	  std::thread(std::bind(&LidarPlugin::QueueThread, this));
-    }
     
-    /*public: void SetPosition(const double &_pos)
-    {
-     // Set the joint's target position.
-      this->model->GetJointController()->SetPositionTarget(
-          this->joint1->GetScopedName(), _pos);     
     }
-    
-    private: void OnMsg(ConstVector3dPtr &_msg)
-    {
-        this->SetPosition(_msg->x());
-    }
-    
-    public: void OnRosMsg(const std_msgs::Float32ConstPtr &_msg)
-    {
-    	std::cerr << "Joint 1: ";
-    	std::cerr << _msg->data;
-       this->SetPosition(_msg->data);
-    }*/
     
     public: void SetPosition(const int &_jn, const double &_pos)
     {
      //Chosing the joint
      this->joint1 = model->GetJoints()[_jn];
-     // Set the joint's target position.
+     // Set joint's target position.
       this->model->GetJointController()->SetPositionTarget(
           this->joint1->GetScopedName(), _pos);  
           std::cerr << "Joint: " << _jn << " ";
@@ -175,8 +130,7 @@ namespace gazebo
     }
 
 	public: void SetPID1(const int &_P, const double &_I,const int &_D)
-    {
-     //Chosing the pid    
+    {   
 		this->pid1 = common::PID(_P, _I, _D);
 		this->model->GetJointController()->SetPositionPID(
 	      this->joint1->GetScopedName(), this->pid1);
@@ -185,7 +139,6 @@ namespace gazebo
 
 	public: void SetPID2(const int &_P, const double &_I,const int &_D)
     {
-     //Chosing the pid
 		this->pid2 = common::PID(_P, _I, _D);
 	 	this->model->GetJointController()->SetPositionPID(
 	      this->joint2->GetScopedName(), this->pid2);
@@ -195,6 +148,7 @@ namespace gazebo
     private: void OnMsgPos(ConstVector3dPtr &_msg)
     {
         this->SetPosition(_msg->x(), _msg->y());
+		this->Message.Set(_msg->x(), _msg->y());
     }
 
 	private: void OnMsgPID1(ConstVector3dPtr &_msg)
@@ -207,7 +161,6 @@ namespace gazebo
         this->SetPID2(_msg->x(), _msg->y(), _msg->z());
     }
 
-    
     public: void OnRosMsg(const std_msgs::Float32MultiArrayConstPtr &_msg)
     {
     	std::cerr << "Joint 1: ";
@@ -226,7 +179,24 @@ namespace gazebo
           this->rosQueue.callAvailable(ros::WallDuration(timeout));
        }
     }
-    	
+
+	public: void OnUpdate()
+    {
+	ignition::math::Vector2d JointTarPos;
+      // Get current joint position
+	  ignition::math::Vector2d JointPos(this->joint1->Position(), this->joint2->Position());
+	  if (this->Message[0]==0) //if Message = 0 joint 1
+	  {
+		  JointTarPos.Set(this->Message[1], 0);
+		  //std::cout << JointTarPos[0] << "-" << JointPos[0]<< " ";
+	  	  //std::cerr << "Error 1: " <<  JointTarPos[0] - JointPos[0]<< "\r\n";
+	  }else{//else joint 2
+		  JointTarPos.Set(0, this->Message[1]);
+		  //std::cout << JointTarPos[1] << "-" << JointPos[1]<< " ";
+	  	  //std::cerr << "Error 2: " <<  JointTarPos[1] - JointPos[1]<< "\r\n";
+	  }
+    }
+
 	/// \brief A node used for transport
 	private: transport::NodePtr nodePos;
 	private: transport::NodePtr nodePID;
@@ -236,7 +206,7 @@ namespace gazebo
 	private: transport::SubscriberPtr subPID1;
 	private: transport::SubscriberPtr subPID2;
 
-    	/// \brief Pointer to the model.
+    /// \brief Pointer to the model.
 	private: physics::ModelPtr model;
 
 	/// \brief Pointer to the joint.
@@ -247,6 +217,9 @@ namespace gazebo
 	private: common::PID pid1;
 	private: common::PID pid2;
 	private: common::PID GetErrors;
+
+	// Pointer to the update event connection
+    private: event::ConnectionPtr updateConnection;
 	
 	/// \brief A node use for ROS transport
 	private: std::unique_ptr<ros::NodeHandle> rosNode;
@@ -259,6 +232,9 @@ namespace gazebo
 
 	/// \brief A thread the keeps running the rosQueue
 	private: std::thread rosQueueThread;
+
+	///Message variable
+	private: ignition::math::Vector2d Message;
   };
 
   // Tell Gazebo about this plugin, so that Gazebo can call Load on this plugin.
